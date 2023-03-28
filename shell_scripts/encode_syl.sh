a52ae46ec469f1167197e32a7249f3a1b34deaa6
Marco Ponzi <marco.ponzi@gmail.com>
2023-03-28 08:26:42 +0200

    changes to bg and syl encodings

--- shell_scripts/encode_syl.sh
+++ shell_scripts/encode_syl.sh
@@ -13,7 +13,7 @@ done | sort -rnk 2 | head -10 | grep -o '^.' > tmp/top_chars
 for c in `cat tmp/top_chars`
 do
   #cat tmp/out1 | sed -e 's/\(0_.0_.0_.0_.0_.0_.0\)_'$c'0/\1_'$c'1/g' | sed -e 's/_$//' > tmp/out2
-  cat tmp/out1 | sed -e 's/\('$c'0[^'$c']*\)_'$c'0/\1_'$c'1/g' | sed -e 's/_$//' > tmp/out2
+  cat tmp/out1 | sed -e 's/\('$c'0[^'$c']*\)_'$c'0/\1_'$c'1/g' | sed -e 's/_*$/_/' > tmp/out2
   mv tmp/out2 tmp/out1
 done
 
