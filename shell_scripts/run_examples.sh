426a5ab62dc95b6519b6e0bf82852f1d12a420cc
Marco Ponzi <marco.ponzi@gmail.com>
2023-12-22 19:07:59 +0100

    script for deciphering examples of different ciphers

---
+++ shell_scripts/run_examples.sh
@@ -0,0 +1,27 @@
+
+RESTARTS=400
+
+echo 
+echo lat texts/examples/desine.lat.simple simplesub 50
+time python3.6 yablasto.py lat texts/examples/desine.lat.simple simplesub 50  > tmp/out.simplesub
+cat tmp/out.simplesub | tail -10
+
+echo
+echo eng texts/examples/thesearethegenerations_long.eng.syl syl $RESTARTS
+time python3.6 yablasto.py eng texts/examples/thesearethegenerations_long.eng.syl syl $RESTARTS > tmp/out.syl
+cat tmp/out.syl | tail -10
+
+echo
+echo lat texts/examples/desine.lat.nulls nulls $RESTARTS
+time python3.6 yablasto.py lat texts/examples/desine.lat.nulls nulls $RESTARTS  > tmp/out.nulls
+cat tmp/out.nulls | tail -10
+
+echo 
+echo lat texts/examples/desine.lat.verbose verbosebigr $RESTARTS
+time python3.6 yablasto.py lat texts/examples/desine.lat.verbose verbosebigr $RESTARTS > tmp/out.verbosebigr
+cat tmp/out.verbosebigr | tail -10
+
+
+
+
+
