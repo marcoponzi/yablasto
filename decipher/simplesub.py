ff6dc7abc4248ec4846a8aa6187ebf239c2465df
Marco Ponzi <marco.ponzi@gmail.com>
2023-12-23 09:09:26 +0100

    check you are swapping values of different keys

--- decipher/simplesub.py
+++ decipher/simplesub.py
@@ -25,6 +25,7 @@ def change_key(key, cipher_text, plain_alphabet):
     klist=list(key.keys())
     cipher_alphabet=sorted(list(set(list(cipher_text))))
     
+    # plain_alphabet larger than cipher_alphabet?
     diff=set(plain_alphabet)-set(key.values())
     
     if len(diff)>0 and random.random()>.2: #pick unused plain character
@@ -32,7 +33,7 @@ def change_key(key, cipher_text, plain_alphabet):
     else: #swap two values
       k1=random.choice(klist)
       k2=random.choice(klist)
-      while key[k2]==key[k1]:
+      while k2==k1 or key[k2]==key[k1]:
         k2=random.choice(klist)
       temp=key[k1]
       key[k1]=key[k2]
