076b4f6f38939d941a434f2f0e7b18c3bd1b756a
Marco Ponzi <marco.ponzi@gmail.com>
2024-02-04 08:43:35 +0100

    reduced number of verbose tokens with nulls

--- shell_scripts/encode_verbosenulls.sh
+++ shell_scripts/encode_verbosenulls.sh
@@ -1,11 +1,34 @@
-sed -e 's/o/SE/g' |\
-sed -e 's/e/ST/g' |\
-tr h  1 |\
-tr y  O |\
-tr a  0 |\
+tr '\n' ' ' > tmp/res.txt
+
+patt='...............'
+for c in 3 4 # nulls
+do
+cat tmp/res.txt | sed -e 's/\('$patt'\)/\1'$c'/g' > tmp/res1.txt
+mv tmp/res1.txt tmp/res.txt
+patt=$patt'.......................'
+done
+
+# 5 and 6 once (nulls)
+cat tmp/res.txt | sed -e 's/\(...........\)/\16/'  | sed -e 's/\(..\)/\15/'> tmp/res1.txt
+mv tmp/res1.txt tmp/res.txt
+
+#for c in 5 6 # once
+#do
+#cat tmp/res.txt | sed -e 's/\('$patt'\)/\1'$c'/' > tmp/res1.txt
+#mv tmp/res1.txt tmp/res.txt
+#patt=$patt'...........'
+#done
+
+# h a
+cat tmp/res.txt |\
+sed -e 's/h/SO/g' |\
+sed -e 's/a/22/g' |\
+tr o  O |\
+tr y  T |\
 tr c  H |\
 tr d  N |\
-sed -e 's/i/SS/g' |\
+tr e  E |\
+tr i  1 |\
 tr k  R |\
 tr l  D |\
 tr r  L |\
@@ -23,21 +46,4 @@ tr z  V |\
 tr w  Q |\
 tr u  J |\
 tr j  X |\
-tr b  Z  | tr [:upper:] [:lower:] | tr '\n' ' ' > tmp/res.txt
-
-patt='...............'
-for c in 2 3 
-do
-cat tmp/res.txt | sed -e 's/\('$patt'\)/\1'$c'/g' > tmp/res1.txt
-mv tmp/res1.txt tmp/res.txt
-patt=$patt'..........'
-done
-
-for c in 4 5 6 # once
-do
-cat tmp/res.txt | sed -e 's/\('$patt'\)/\1'$c'/' > tmp/res1.txt
-mv tmp/res1.txt tmp/res.txt
-patt=$patt'.......'
-done
-
-cat tmp/res.txt
+tr b  Z | tr [:upper:] [:lower:] 
