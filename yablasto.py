46cf089dbd0e4193ba4b0b0dcdef3d08fa97c389
Marco Ponzi <marco.ponzi@gmail.com>
2025-02-09 10:01:23 +0100

    simple substitution with word anagrams

--- yablasto.py
+++ yablasto.py
@@ -12,6 +12,7 @@ import datetime
 #from split_into_words import word_break_with_gaps
 
 import cipher.simplesub as simplesub
+import cipher.simplesubanagr as simplesubanagr
 import cipher.verbosebigr as verbosebigr
 import cipher.nulls as nulls
 import cipher.verbosenulls as verbosenulls
@@ -48,7 +49,7 @@ def load_quadgrams(lang):
     return store
 
 
-def parse_qgram(lang):
+def parse_qgram(lang, is_anagr):
     # create dict of ngram strings : ints
     ngrams = {}
     plain_alphabet=set()
@@ -66,7 +67,11 @@ def parse_qgram(lang):
     # generate all 4-gram combos from A-Z
     # if combo not in ngram dict then add it
     chars = string.ascii_uppercase
+    if is_anagr:
+      chars=chars+','
+    print("Chars: "+str(chars))
     orderings = list(itertools.product(chars, repeat=4))
+    #print(orderings[:100])
     cntr = 0
     for i in orderings:
         it = ''.join(i)
@@ -94,9 +99,13 @@ def parse_qgram(lang):
         qlf = v / ( qtot + cntr)
         log = log10(float( qlf ) )
         qgram.append(log)
+    if is_anagr:
+      assert len(qgram) == 531441, 'qgram anagram list size error'
+    else:
+      assert len(qgram) == 456976, 'qgram list size error'
 
-    assert len(qgram) == 456976, 'qgram list size error'
-
+    if ',' in plain_alphabet:
+      plain_alphabet.remove(',')
     return qgram, list(plain_alphabet)
 
 
@@ -105,11 +114,15 @@ def clean_input(ciphertext):
     text = re.sub("[ \n]","",ciphertext)
     return text
 
-
+def decrypt2(cipher_text,parent_key, is_anagr):
+  plain_text=cipher_utils.decrypt(cipher_text,parent_key)
+  if is_anagr:
+    plain_text=simplesubanagr.do_anagrams(plain_text)
+  return plain_text
 
 def hill_climbing(cipher_text, plateau, sleep, parent_key,perc_progress, module):
     child_key = ""
-    best_score, ignore = score_text(cipher_utils.decrypt(cipher_text,parent_key ), module)
+    best_score, ignore = score_text(decrypt2(cipher_text,parent_key, is_anagr ), module)
     ## best_score = -99999 #-72.6613499895892
     best_key=parent_key # -85.74
     parent_score=best_score
@@ -119,7 +132,7 @@ def hill_climbing(cipher_text, plateau, sleep, parent_key,perc_progress, module)
     consec_fails = 0
     while GO:
         child_key = module.change_key(copy.deepcopy(parent_key),cipher_text, plain_alphabet)
-        child_score, ignore = score_text(cipher_utils.decrypt(cipher_text,child_key ), module)
+        child_score, ignore = score_text(decrypt2(cipher_text,child_key, is_anagr ), module)
         if child_score > best_score or (child_score==best_score and len(child_key)<len(best_key)):
           ### log("child better: "+str(child_score)+" best: "+str(best_score))
           consec_fails = 0
@@ -152,43 +165,57 @@ def hill_climbing(cipher_text, plateau, sleep, parent_key,perc_progress, module)
     log("curr_parent: "+str(parent_score)+" "+cipher_utils.key_to_str(parent_key))
     result=dict()
     result['key'] = best_key
-    result['plain'] = cipher_utils.decrypt(cipher_text, best_key)
+    result['plain'] = decrypt2(cipher_text, best_key, is_anagr)
     result['score'] = best_score
     return result
 
-
+def get_quad_val(alpha,text,i,temp,l,l2,l3):
+        temp[0] = alpha.find(text[i])
+        temp[1] = alpha.find(text[i+1])
+        temp[2] = alpha.find(text[i+2])
+        temp[3] = alpha.find(text[i+3])
+        pos=(l3)*temp[0] + (l2)*temp[1] + l*temp[2] + temp[3]
+        val=qgram[pos]
+        return val,pos
 
 
 #####################
 def score_text(text, module):
     # [M] handling unreplaced characters as Z TODO
-    text=re.sub('[^A-Z]','@',text)
-    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+    # comma , as word separator for anagrams
+    text=re.sub('[^A-Z, ]','@',text)
+    if is_anagr:
+      text=simplesubanagr.do_anagrams(text.upper().replace(' ',','))
+      alpha = ",ABCDEFGHIJKLMNOPQRSTUVWXYZ" # comma ',' word separator
+    else:
+      alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+     
     temp = [0,0,0,0]
     score = 0
-    #l = len(alpha)
-    l=26
+    l=len(alpha)
     l3 = l**3
     l2 = l**2
-    #print("text: "+text)
+    best_val=-99999999
+    best_quad='____'
     for i in range(0,len(text)-3):
+      val=-5*len(text)
       if '@' in text[i:i+4]:
         score-=len(text)*5 # unrecognized characters
-        #print("score minus "+str(score))
       else:
-        temp[0] = alpha.find(text[i])
-        temp[1] = alpha.find(text[i+1])
-        temp[2] = alpha.find(text[i+2])
-        temp[3] = alpha.find(text[i+3])
-        val=qgram[(l3)*temp[0] + (l2)*temp[1] + l*temp[2] + temp[3]]
+        val,pos=get_quad_val(alpha,text,i,temp,l,l2,l3)
         score += val**3
-        #print("score plus "+str(score))
+      ## print("score plus "+text[i:i+4]+" "+str(score))
     quad_score=score/float(max(0.001,len(text)-3)) # max: avoid division by zero
-    #print("SCORE "+str(score)+"\n\n")
+    ##print("SCORE "+str(score)+" "+best_quad+" "+str(best_val))
     return module.score(quad_score,text),quad_score
     
 
-
+def evaluate_by_lexicon2(plain_text,lexicon, longest_word, lexicon_avg_len, is_anagr):
+  print(plain_text)
+  if is_anagr:
+    plain_text=simplesubanagr.do_anagrams(plain_text.upper().replace(' ',','))
+    print("ANAGR:: "+plain_text)
+  return cipher_utils.evaluate_by_lexicon(plain_text, lexicon, longest_word, lexicon_avg_len, True)
 
 
 #############################################
@@ -211,13 +238,11 @@ with open(ARG_CTEXT_FILE, "r") as infile:
   for line in infile:
     ctext+=line
 
-qgram,plain_alphabet = parse_qgram(ARG_LANG)
-
-
-cipher_text = clean_input(ctext)
-log('cipher text:'+cipher_text)
-log(' ')
-
+is_anagr=(ARG_MODULE=='simplesubanagr')
+if is_anagr:
+  qgram,plain_alphabet = parse_qgram(ARG_LANG,True)
+else:
+  qgram,plain_alphabet = parse_qgram(ARG_LANG,False)
 
 log(' ')
 
@@ -241,6 +266,8 @@ if ARG_MODULE.startswith('crib_'):
     crib_module.set_lexicon(lexicon, longest_word, lexicon_avg_len)
 elif ARG_MODULE=='simplesub':
   module=simplesub
+elif ARG_MODULE=='simplesubanagr':
+  module=simplesubanagr
 elif ARG_MODULE=='verbosebigr':
   module=verbosebigr
 elif ARG_MODULE=='verbosenulls':
@@ -253,10 +280,18 @@ elif ARG_MODULE=='syl':
 else:
   log("Unknown module "+ARG_MODULE)
 
+if is_anagr:
+  cipher_text=re.sub('[\n ]*$','',ctext)
+  cipher_text = cipher_text.replace(' ',',') # comma , as word separator
+else:
+  cipher_text = clean_input(ctext)
+log('cipher text:'+cipher_text)
+log(' ')
+
 if ARG_RESTARTS=='score':
 
   lexicon, longest_word, lexicon_avg_len=cipher_utils.load_lexicon(ARG_LANG)
-  cipher_utils.evaluate_by_lexicon(cipher_text.upper(), lexicon, longest_word, lexicon_avg_len, True)
+  evaluate_by_lexicon2(cipher_text.upper(), lexicon, longest_word, lexicon_avg_len, is_anagr)
   tot_score, quad_score=score_text(cipher_text.upper(),module)
   print("QUAD_SCORE: "+frmt(quad_score)+" TOT_SCORE: "+frmt(tot_score))
   sys.exit()
@@ -276,6 +311,10 @@ for restart in range(restarts ):
      if perc_progress<=.15: # .1 initial random keys
         parent_key = module.init_key(cipher_text, plain_alphabet)
         log("")
+        initial_out=decrypt2(cipher_text,parent_key, is_anagr )
+        best_score, ignore = score_text(initial_out, module)
+        log("initial score:"+str(best_score))
+        evaluate_by_lexicon2(initial_out, lexicon, longest_word, lexicon_avg_len, is_anagr)
         log("RAND KEY "+cipher_utils.key_to_str(parent_key))
      else: # refining current best key
         parent_key=copy.deepcopy(best_res['key'])
@@ -305,7 +344,7 @@ log(' ')
 best_plain=best_res['plain']
 log("best_plain: "+str(best_plain))
 lexicon, longest_word, lexicon_avg_len=cipher_utils.load_lexicon(ARG_LANG)
-cipher_utils.evaluate_by_lexicon(best_plain, lexicon, longest_word, lexicon_avg_len, True)
+evaluate_by_lexicon2(best_plain.upper(), lexicon, longest_word, lexicon_avg_len, is_anagr)
 tot_score, quad_score=score_text(best_plain,module)
 print("QUAD_SCORE: "+frmt(quad_score)+" TOT_SCORE: "+frmt(tot_score))
 
