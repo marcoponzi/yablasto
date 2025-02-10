d334a3f410f658807d29dd957db606c2fa401f90
Marco Ponzi <marco.ponzi@gmail.com>
2025-02-10 07:41:15 +0100

    removed duplicate entry

--- shell_scripts/run_examples.sh
+++ shell_scripts/run_examples.sh
@@ -1,4 +1,4 @@
-
+mkdir tmp
 SHORT=150
 LONG=300  
 
@@ -33,13 +33,6 @@ python3.6 yablasto.py eng.uv texts/examples/thesearethegenerations_long.eng.uv v
 time python3.6 yablasto.py eng.uv texts/examples/thesearethegenerations_long.eng.uv.verbosebigr verbosebigr $LONG 0.05 >\
      tmp/out.verbosebigr
 cat tmp/out.verbosebigr | tail -10
-
-echo
-echo eng texts/examples/thesearethegenerations_long.eng.verbosenulls verbosenulls $SHORT 2
-python3.6 yablasto.py eng texts/examples/thesearethegenerations_long.eng verbosenulls score | tail -1
-time python3.6 yablasto.py eng texts/examples/thesearethegenerations_long.eng.verbosenulls verbosenulls $SHORT 2  >\
-	     tmp/out.verbosenulls
-cat tmp/out.verbosenulls | tail -10
 
 echo
 echo eng texts/examples/thesearethegenerations_long.eng.verbosenulls verbosenulls $SHORT 2
